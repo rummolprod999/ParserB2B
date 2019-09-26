1120803b3d0c84ca68a3a0777a9f5652892bd01d
rummolprod999 <rummolprod999@gmail.com>
2019-09-26 11:00:45 +0300

    fix issues

--- parser.go
+++ parser.go
@@ -29,7 +29,7 @@ func Parser() {
 		Logging("Ошибка при парсинге строки", err)
 		return
 	}
-	var Dsn = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=true&readTimeout=60m&maxAllowedPacket=0&timeout=60m&writeTimeout=60m&autocommit=true&loc=Local", UserDb, PassDb, DbName)
+	var Dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=true&readTimeout=60m&maxAllowedPacket=0&timeout=60m&writeTimeout=60m&autocommit=true&loc=Local", UserDb, PassDb, Server, Port, DbName)
 	db, err := sql.Open("mysql", Dsn)
 	defer db.Close()
 	db.SetConnMaxLifetime(time.Second * 3600)
